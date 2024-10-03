7628be5f7b6b9ca7ba8e63487faeb53272a51c6e
cris soto <criscodesnyc@gmail.com>
2024-10-03 16:35:10 -0400

    all

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,40 +1,38 @@
-import "./mainpage.scss"
+import "./mainpage.scss";
 import { init } from 'ityped';
-import {useEffect, useRef} from 'react';
+import { useEffect, useRef } from 'react';
 
 function MainPage() {
     const textRef = useRef();
 
-    useEffect(()=>{
-        init(textRef.current,{
+    useEffect(() => {
+        init(textRef.current, {
             showCursor: false,
             loop: false,
-            backSpeed:80,
+            backSpeed: 80,
             backDelay: 1500,
             disableBackTyping: true,
             strings: ["Hello, World!"]
-        })
+        });
     }, []);
-    
+
     return (
-    <section id='mainpage'>
-        <div className="mainpage">
-            <div className="left">
-                <div className="imgContainer">
-                    <img src="assets/me.png"></img>
+        <section id='mainpage'>
+            <div className="mainpage">
+                <div className="left">
+                    <div className="imgContainer">
+                        <img src="assets/me.png" alt="Profile" />
+                    </div>
                 </div>
-            </div>
-            <div className="right">
-                <div className="rwrapper">
+                <div className="right">
                     <h1 ref={textRef}></h1>
                     <h2>
-                        I'm Cris,<br/>
+                        I'm Cris,<br />
                         A Data Engineer.
                     </h2>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
     );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,13 @@
+import React, { useState } from 'react';
 import './navbar.scss';
 
-function NavBar(){
+function NavBar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div className='navbar'>
             <div className='wrapper'>
@@ -9,18 +16,23 @@ function NavBar(){
                         Cristopher Soto 
                     </a>
                 </div>
-                <div className='right'>
+                <div className={`right ${isMenuOpen ? 'active' : ''}`}>
                     <div className='titles'>
-                    <a href="#aboutme">
-                        About Me
-                    </a>
-                    <a href="#projects">
-                        Projects 
-                    </a>
-                    <a href="#resume">
-                        Resume
-                    </a>
+                        <a href="#aboutme" onClick={() => setIsMenuOpen(false)}>
+                            About Me
+                        </a>
+                        <a href="#projects" onClick={() => setIsMenuOpen(false)}>
+                            Projects 
+                        </a>
+                        <a href="#resume" onClick={() => setIsMenuOpen(false)}>
+                            Resume
+                        </a>
                     </div>
+                </div>
+                <div className={`hamburger ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </div>
             </div>
         </div>
